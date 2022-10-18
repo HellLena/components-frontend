850053028f5439d749355d4c79e7d38c0bef9c7d
HellLena <longlivehard.n.heavy@gmail.com>
2022-10-18 09:36:23 -0400

    Update unit types UI

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UnitList, UnitShow, UnitEdit } from './units';
 import { UnitTypeList, UnitTypeShow, UnitTypeCreate, UnitTypeEdit } from './unitTypes';
+import Apps from '@mui/icons-material/Apps';
+import ListIcon from '@mui/icons-material/List';
 
 const dataProvider = jsonServerProvider('http://localhost:8080');
 
 const App = () => (
-    <Admin dataProvider={dataProvider}>
-        <Resource name="units" list={ UnitList } show={ UnitShow } edit={ UnitEdit } />
-        <Resource name="unit-types" list={ UnitTypeList } show={ UnitTypeShow } create={ UnitTypeCreate } edit={ UnitTypeEdit } />
+    <Admin dataProvider={dataProvider} title="ЦУП" >
+        <Resource 
+            name="units" 
+            options={{ label: 'Сборочные единицы' }}
+            icon={ Apps }
+            list={ UnitList } 
+            show={ UnitShow } 
+            edit={ UnitEdit } />
+        <Resource 
+            name="unit-types" 
+            options={{ label: 'Типы сборочных единиц' }}
+            icon={ ListIcon }
+            list={ UnitTypeList } 
+            show={ UnitTypeShow } 
+            create={ UnitTypeCreate } 
+            edit={ UnitTypeEdit } />
     </Admin>
 );
 
--- src/units.js
+++ src/units.js
@@ -1,26 +1,42 @@
 import {
-    TopToolbar, CreateButton, ExportButton, List, Show, Datagrid, NumberField, TextField, DateField, BooleanField,
-    EditButton, ShowButton, SimpleShowLayout, Edit, SimpleForm, ReferenceInput, SelectInput, TextInput, required,
-    useCreatePath
+    TopToolbar, CreateButton, List, Show, Datagrid, NumberField, TextField, DateField,
+    EditButton, SimpleShowLayout, Edit, SimpleForm, ReferenceInput, SelectInput, TextInput, required,
+    useCreatePath, WrapperField, useRecordContext
 } from 'react-admin';
 import { Link } from "react-router-dom"
 import Box from '@mui/material/Box'
+import CircleIcon from '@mui/icons-material/Circle'
 
 const UtilListActions = () => (
     <TopToolbar>
         <CreateButton/>
-        <ExportButton/>
     </TopToolbar>
 );
+
+const statusColors = {
+    "NEW": "#ffbc21",
+    "PROCESSING": "#ffbc21",
+    "FAILED": "#ffbc21",
+    "MODERATION": "#ffbc21",
+    "APPROVED": "#ffbc21"
+}
+
+const StatusIcon = (props) => {
+    const record = useRecordContext(props);
+    return <CircleIcon sx={{ color: statusColors[record.bomFileStatus.name] }} fontSize="10px" />;
+};
 
 export const UnitList = () => (
     <List actions={<UtilListActions/>}>
         <Datagrid rowClick="show">
             <NumberField source="id" />
-            <TextField source="unitType" sortBy="unit_type_id" />
-            <TextField source="decimalName" sortBy="decimal_name" />
-            <TextField source="bomFileStatus" sortBy="bom_file_status" />
-            <DateField source="createdAt" sortBy="created_at" />
+            <TextField source="unitType" sortBy="unit_type_id" label="Тип сборочной единицы" />
+            <TextField source="decimalName" sortBy="decimal_name" label="Децимальный номер" />
+            <WrapperField label="Статус BoM файла">
+                <StatusIcon source="bomFileStatus.name" />&nbsp;
+                <TextField source="bomFileStatus.description" sortBy="bom_file_status" />
+            </WrapperField>
+            <DateField source="createdAt" sortBy="created_at" label="Дата создания" locales="ru-RU" showTime={true} />
             <EditButton/>
         </Datagrid>
     </List>
@@ -30,10 +46,13 @@ export const UnitShow = () => (
     <Show>
         <SimpleShowLayout>
             <NumberField source="id" label="Уникальный идентификатор" />
-            <TextField source="unitType" label="Тип сборочной единицы" />
+            <TextField source="unitType.name" label="Тип сборочной единицы" />
             <TextField source="decimalName" label="Децимальный номер" />
-            <TextField source="bomFileStatus" label="Статус BoM файла" />
-            <DateField source="createdAt" label="Создана" />
+            <WrapperField label="Статус BoM файла">
+                <StatusIcon source="bomFileStatus.name" />&nbsp;
+                <TextField source="bomFileStatus.description" sortBy="bom_file_status" />
+            </WrapperField>
+            <DateField source="createdAt" label="Дата создания" locales="ru-RU" showTime={true} />
         </SimpleShowLayout>
     </Show>
 );
@@ -46,12 +65,13 @@ export const UnitEdit = () => {
                 <Box display={{ xs: 'block', sm: 'flex', width: '100%'}}>
                     <Box flex={1} mr={{ xs: 0, sm: '0.5em'}}>
                         <ReferenceInput
-                                source="unit_type_id"
-                                reference="unit-types">
-                            <SelectInput
-                                label="Тип сборочной единицы"
+                                source="id"
+                                reference="unit-types"
+                                sort={{ field: 'name' }}>
+                            <SelectInput 
+                                label="Тип сборочной единицы" 
                                 optionText="name"
-                                validate={required()} 
+                                validate={required()}
                                 fullWidth/>
                         </ReferenceInput>
                     </Box> 
